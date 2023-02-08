7061eb6d159a9cd365d24474587a3491c0ea4cd9
Michael <michaeljsto76@gmail.com>
2023-02-08 14:16:32 -0600

    Wrote a ton of comments (in models.py urls.py and views.py) hopefully explaining what everything is well enough for me to talk about and explain fairly well.

--- mysite/new_site/models.py
+++ mysite/new_site/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 
 class Name(models.Model):
+    ## A model in django takes data and saves it to a database (help users do less work to properly save data) 
+    ## name_text is just a text box asking the user to imput their name.
     name_text = models.CharField(max_length=200)
     time_played = models.CharField(max_length=5)
 
 class Rating(models.Model):
     name = models.ForeignKey(Name, null = True, on_delete=models.CASCADE)
     user_rating = models.IntegerField(default = 1) 
+
+## A object in python has Atributes (Data) and Methods (Functions)
+## A Class is a generalization of the type that an object is
